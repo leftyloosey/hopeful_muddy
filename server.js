f26a547cf6f0414611fa783bc97c989d40a38f37
davidhardin <thisgoestodavid@gmail.com>
2022-09-30 20:25:45 -0600

    helloku

--- server.js
+++ server.js
@@ -95,11 +95,11 @@ app.use(
   );
 
   // app.use(express.static(path.join(__dirname, '../client/build')));
-// app.use(express.static('public'));
+app.use(express.static('public'));
 // app.use(express.static('client-app/build'));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 
 app.listen(port, console.log(`server running on ${port}`))
