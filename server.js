bc0fb89a64a9478cc63ff7c362ac02ab9ef9ef37
davidhardin <thisgoestodavid@gmail.com>
2022-09-30 18:14:14 -0600

    herokuuuu

--- server.js
+++ server.js
@@ -95,10 +95,10 @@ app.use(
   );
 
 
-// app.use(express.static('public'));
+app.use(express.static('public'));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 
 app.listen(port, console.log(`server running on ${port}`))
